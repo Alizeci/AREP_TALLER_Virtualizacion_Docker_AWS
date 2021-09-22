32df2b1f78ed100b5d9fe35e9889b25289dc7655
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-09-22 15:55:51 -0500

    Adding MongoDB connection to LogService

--- src/main/java/co/edu/escuelaing/sparkweb/dockerdemolab/persistence/LogServiceMongoDB.java
+++ src/main/java/co/edu/escuelaing/sparkweb/dockerdemolab/persistence/LogServiceMongoDB.java
@@ -1,6 +1,22 @@
 package co.edu.escuelaing.sparkweb.dockerdemolab.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 public class LogServiceMongoDB {
+
+	public static void main(String[] args) {
+		String connectionString = System.getProperty("mongodb://localhost:27017");
+		try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+			List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+			databases.forEach(db -> System.out.println(db.toJson()));
+		}
+	}
 
 	public String createString(String string) {
 		return string;
